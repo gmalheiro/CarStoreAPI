bc6178d3142cd6a05c5a241990fa7eb32cede3cc
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-09 00:57:21 -0300

    Created db migrations and datasets

---
+++ CarStoreAPI/Data/Db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO Person (Name, Surname, Birthdate, DocumentNumber)
+VALUES
+    ('John', 'Doe', '01/05/1985', '44 778 999-6'),
+    ('Jane', 'Smith', '15/10/1990', '55 123 456 7'),
+    ('Bob', 'Johnson', '20/03/1978', '33 987 654 2');
