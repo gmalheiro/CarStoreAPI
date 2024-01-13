686230953d38c05b56d6fafe9f0f75029830d25d
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-13 08:44:50 -0300

    Using top level statements in controllers

--- CarStoreAPI/Controllers/CarController.cs
+++ CarStoreAPI/Controllers/CarController.cs
@@ -2,58 +2,57 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CarStoreAPI.Controllers
+namespace CarStoreAPI.Controllers;
+
+[Route("api/[controller]/v1")]
+[ApiController]
+public class CarController : ControllerBase
 {
-    [Route("api/[controller]/v1")]
-    [ApiController]
-    public class CarController : ControllerBase
+    [HttpGet("FindAll")]
+    [ProducesResponseType(typeof(List<Car>), 200)]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public ActionResult FindAll()
     {
-        [HttpGet("FindAll")]
-        [ProducesResponseType(typeof(List<Car>), 200)]
-        [ProducesResponseType(204)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(401)]
-        public ActionResult FindAll()
-        {
-            return Ok();
-        }
+        return Ok();
+    }
 
-        [HttpGet("FindById/{id}")]
-        [ProducesResponseType(typeof(Car), 200)]
-        [ProducesResponseType(204)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(401)]
-        public ActionResult FindById()
-        {
-            return Ok();
-        }
+    [HttpGet("FindById/{id}")]
+    [ProducesResponseType(typeof(Car), 200)]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public ActionResult FindById()
+    {
+        return Ok();
+    }
 
-        [HttpPost]
-        [ProducesResponseType(typeof(Car), 200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(401)]
-        public ActionResult Create()
-        {
-            return Ok();
-        }
-        
-        [HttpPut]
-        [ProducesResponseType(typeof(Car), 200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(401)]
-        public IActionResult Put([FromBody] Car car)
-        {
-            return Ok(car);
-        }
-        
-        [HttpDelete("Delete/{id}")]
-        [ProducesResponseType(typeof(Car), 200)]
-        [ProducesResponseType(204)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(401)]
-        public IActionResult Delete(int id)
-        {
-            return NoContent();
-        }
+    [HttpPost]
+    [ProducesResponseType(typeof(Car), 200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public ActionResult Create()
+    {
+        return Ok();
+    }
+    
+    [HttpPut]
+    [ProducesResponseType(typeof(Car), 200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public IActionResult Put([FromBody] Car car)
+    {
+        return Ok(car);
+    }
+    
+    [HttpDelete("Delete/{id}")]
+    [ProducesResponseType(typeof(Car), 200)]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public IActionResult Delete(int id)
+    {
+        return NoContent();
     }
 }
--- CarStoreAPI/Controllers/PersonController.cs
+++ CarStoreAPI/Controllers/PersonController.cs
@@ -2,58 +2,57 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CarStoreAPI.Controllers
+namespace CarStoreAPI.Controllers;
+
+[Route("api/[controller]/v1")]
+[ApiController]
+public class PersonController : ControllerBase
 {
-    [Route("api/[controller]/v1")]
-    [ApiController]
-    public class PersonController : ControllerBase
+    [HttpGet("FindAll")]
+    [ProducesResponseType(typeof(List<Person>), 200)]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public ActionResult FindAll()
     {
-        [HttpGet("FindAll")]
-        [ProducesResponseType(typeof(List<Person>), 200)]
-        [ProducesResponseType(204)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(401)]
-        public ActionResult FindAll()
-        {
-            return Ok();
-        }
+        return Ok();
+    }
 
-        [HttpGet("FindById/{id}")]
-        [ProducesResponseType(typeof(Person), 200)]
-        [ProducesResponseType(204)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(401)]
-        public ActionResult FindById()
-        {
-            return Ok();
-        }
+    [HttpGet("FindById/{id}")]
+    [ProducesResponseType(typeof(Person), 200)]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public ActionResult FindById()
+    {
+        return Ok();
+    }
 
-        [HttpPost("Create")]
-        [ProducesResponseType(typeof(Person), 200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(401)]
-        public ActionResult Create()
-        {
-            return Ok();
-        }
+    [HttpPost("Create")]
+    [ProducesResponseType(typeof(Person), 200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public ActionResult Create()
+    {
+        return Ok();
+    }
 
-        [HttpPut("Update")]
-        [ProducesResponseType(typeof(Person), 200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(401)]
-        public IActionResult Put([FromBody] Person person)
-        {
-            return Ok(person);
-        }
+    [HttpPut("Update")]
+    [ProducesResponseType(typeof(Person), 200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public IActionResult Put([FromBody] Person person)
+    {
+        return Ok(person);
+    }
 
-        [HttpDelete("Delete/{id}")]
-        [ProducesResponseType(typeof(Person), 200)]
-        [ProducesResponseType(204)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(401)]
-        public IActionResult Delete(int id)
-        {
-            return NoContent();
-        }
+    [HttpDelete("Delete/{id}")]
+    [ProducesResponseType(typeof(Person), 200)]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public IActionResult Delete(int id)
+    {
+        return NoContent();
     }
 }
