850c9478ae2b825ea9017e6b39ab42f7f43c343a
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-12 22:16:07 -0300

    Created person controller

--- CarStoreAPI/Controllers/PersonController.cs
+++ CarStoreAPI/Controllers/PersonController.cs
@@ -1,11 +1,59 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CarStoreAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarStoreAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/v1")]
     [ApiController]
     public class PersonController : ControllerBase
     {
+        [HttpGet("FindAll")]
+        [ProducesResponseType(typeof(List<Person>), 200)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public ActionResult FindAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("FindById/{id}")]
+        [ProducesResponseType(typeof(Person), 200)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public ActionResult FindById()
+        {
+            return Ok();
+        }
+
+        [HttpPost("Create")]
+        [ProducesResponseType(typeof(Person), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public ActionResult Create()
+        {
+            return Ok();
+        }
+
+        [HttpPut("Update")]
+        [ProducesResponseType(typeof(Person), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public IActionResult Put([FromBody] Person person)
+        {
+            return Ok(person);
+        }
+
+        [HttpDelete("Delete/{id}")]
+        [ProducesResponseType(typeof(Person), 200)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
     }
 }
