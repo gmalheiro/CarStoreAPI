abb314fb5ca7851c168e3e41671afc17ad42b978
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-14 18:18:42 -0300

    Created infrastructure module

---
+++ CarStoreAPI/Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,23 @@
+﻿using CarStoreAPI.Repository.CarRepositoryDapper;
+using CarStoreAPI.Repository.GenericRepositoryDapper;
+
+namespace CarStoreAPI.Infrastructure;
+
+public static class InfrastructureModule
+{
+    public static IServiceCollection AddDependencies(this IServiceCollection services)
+    {
+        services
+            .AddRepositories();
+        
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<ICarRepositoryDapper, CarRepositoryDapper>();
+
+        return services;
+    }
+
+}
