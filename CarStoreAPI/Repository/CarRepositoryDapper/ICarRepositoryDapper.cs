9856abcec06f120c232e2d5fff3907f349975722
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-14 18:18:33 -0300

    Created class CarRepositoryDapper

---
+++ CarStoreAPI/Repository/CarRepositoryDapper/ICarRepositoryDapper.cs
@@ -0,0 +1,10 @@
+﻿using CarStoreAPI.Models;
+using CarStoreAPI.Repository.GenericRepositoryDapper;
+
+namespace CarStoreAPI.Repository.CarRepositoryDapper;
+
+public interface ICarRepositoryDapper : IGenericRepositoryDapper<Car>
+{
+    void Create(Car car);
+    void Update(Car car);
+}
