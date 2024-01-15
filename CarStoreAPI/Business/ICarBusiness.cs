3a785f962370baa4cc0e51ef3d99798d2fecc46c
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-15 20:56:39 -0300

    Created ICarBusiness

--- CarStoreAPI/Business/ICarBusiness.cs
+++ CarStoreAPI/Business/ICarBusiness.cs
@@ -1,6 +1,12 @@
-﻿namespace CarStoreAPI.Business
+﻿using CarStoreAPI.Data.DTO;
+
+namespace CarStoreAPI.Business;
+
+public interface ICarBusiness
 {
-    public interface ICarBusiness
-    {
-    }
+    List<CarDTO> FindAll();
+    CarDTO FindById(int id);
+    CarDTO Create(CarDTO carDTO);
+    CarDTO Update(CarDTO carDTO);
+    CarDTO DeleteById(int id);  
 }
