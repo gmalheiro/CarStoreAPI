6760320fcc9d13041c308618396566b24c2abfb1
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-15 20:57:51 -0300

    Created CarBusiness

--- CarStoreAPI/Business/Implementations/CarBusiness.cs
+++ CarStoreAPI/Business/Implementations/CarBusiness.cs
@@ -1,6 +1,47 @@
-﻿namespace CarStoreAPI.Business.Implementation
+﻿using CarStoreAPI.Data.DTO;
+using CarStoreAPI.Repository.CarRepositoryDapper;
+
+namespace CarStoreAPI.Business.Implementation;
+
+public class CarBusiness : ICarBusiness
 {
-    public class CarBusiness
+    private readonly ICarRepositoryDapper _carRepositoryDapper;
+
+    public CarBusiness(ICarRepositoryDapper carRepositoryDapper)
     {
+        _carRepositoryDapper = carRepositoryDapper;
+    }
+
+    public CarDTO Create(CarDTO carDTO)
+    {
+        try
+        {
+            return carDTO;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
+
+    public CarDTO DeleteById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<CarDTO> FindAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public CarDTO FindById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public CarDTO Update(CarDTO carDTO)
+    {
+        throw new NotImplementedException();
     }
 }
