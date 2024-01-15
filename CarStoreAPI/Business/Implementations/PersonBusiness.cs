3283527ebed80309387480a0d92f9dd0bad159a1
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-15 20:58:06 -0300

    Created PersonBusiness

--- CarStoreAPI/Business/Implementations/PersonBusiness.cs
+++ CarStoreAPI/Business/Implementations/PersonBusiness.cs
@@ -1,6 +1,57 @@
-﻿namespace CarStoreAPI.Business.Implementation
+﻿using CarStoreAPI.Models;
+using CarStoreAPI.Repository.CarRepositoryDapper;
+using CarStoreAPI.Repository.PersonRepositoryDapper;
+
+namespace CarStoreAPI.Business.Implementation;
+
+public class PersonBusiness : IPersonBusiness
 {
-    public class PersonBusiness
+
+    private readonly IPersonRepository _personRepository;
+
+    public PersonBusiness(IPersonRepository personRepository)
     {
+        _personRepository = personRepository;
+    }
+
+    public Person Create(Person person)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Person Delete(Person person)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Person> FindAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Person FindById(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Person GetOwnerByName(string name)
+    {
+        var person = new Person();
+
+        try
+        {
+            person = _personRepository.GetOwnerByName(name);
+            return person;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+    }
+
+    public Person Update(Person person)
+    {
+        throw new NotImplementedException();
     }
 }
